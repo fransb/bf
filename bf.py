e2813d63e57bb7bae698f941e2e2496bac042d1b
frans <frans.b@gmail.com>
2019-06-20 20:51:47 +0200

    adding more functionality

--- bf.py
+++ bf.py
@@ -1,23 +1,42 @@
 import argparse
 from decimal import *
 from operator import itemgetter
-#102,T=5.738246|3.5607517|1|||359|-151467.48|-65955.93|,Type=Ground+Static+Aerodrome,Name=Oil rig,Color=Red,Coalition=Allies,Country=us
 import json
+import zlib
+import math
+def printTarget(targets, long, lat, targettype, dead):
+    distanses = []
+    for target in targets.values():
+        if "Ground" in target["type"] or "Sea" in target["type"] :
+            found = False
+            
+            if targettype == "a" :
+                found = True
+            elif "AntiAircraft" in target["type"] and targettype == "aa" :
+                found = True
+            elif "Building" in target["type"] and targettype == "b" :
+                found = True
+            if dead or target["status"] == "dead":
+                found = False
+            if found :
+                distance = math.sqrt((target["lat"]-lat)**2+(target["long"]-long)**2)
+                target["distance"]=distance
+                distanses.append(target)
 
-class DecimalEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, Decimal):
-            return float(o)
-        return super(DecimalEncoder, self).default(o)
-
+    distanses.sort(key = sortDist)
+    closest = 0
+    for distance in distanses:
+        if (distance["distance"] < Decimal(0.1)) or closest < 20: 
+            print(distance["status"]+", "+distance["type"]+", "+distance["name"]+", "+str(distance["lat"])+", "+str(distance["long"])+", "+str(distance["alt"])+" m, " + distance["group"]+", "+str(distance["distance"]))
+            closest = closest + 1
 def sortDist(val): 
     return float(val["distance"])
 def parseLine(line, refLat, refLong, file):
     tacobject = dict()
     id = ""
-    long = Decimal(0.0)
-    lat = Decimal(0.0)
-    alt = Decimal(0.0)
+    long = 0.0
+    lat = 0.0
+    alt = 0.0
     type = ""
     name = ""
     color = ""
@@ -25,12 +44,16 @@ def parseLine(line, refLat, refLong, file):
     country = ""
     group = ""
     status = "alive"
+    doPrint = False
     
     for part in line.rstrip().split(","):
         if "T=" in part:
-            long = Decimal(part.split("|")[0].replace("T=",""))+refLong
-            lat = Decimal(part.split("|")[1])+refLat
-            alt = part.split("|")[2]
+            if not part.split("|")[0].replace("T=","") == "":
+                long = float(part.split("|")[0].replace("T=",""))+refLong
+            if not part.split("|")[1] == "":
+                lat = float(part.split("|")[1])+refLat
+            if not part.split("|")[2] == "":
+                alt = float(part.split("|")[2])
         elif "Type=" in part:
             type = part.replace("Type=","")
         elif "Name=" in part:
@@ -56,15 +79,16 @@ def parseLine(line, refLat, refLong, file):
     tacobject["country"] = country
     tacobject["group"] = group
     tacobject["status"] = status
-    if id == "0":
+    if doPrint:
+        print(tacobject)
         print(line)
     
     return tacobject
 def readFile(file, targets):
     linenumber = 0
     fileStarted = False
-    refLong = Decimal(0.00)
-    refLat = Decimal(0.00)
+    refLong = 0.00
+    refLat = 0.00
     time = 0
     print("Reading File: " + file)
     with open(file) as f:
@@ -72,7 +96,7 @@ def readFile(file, targets):
             linenumber = linenumber + 1
             if line[0] == "#":
                 fileStarted = True
-                time = Decimal(line[1:])
+                time = line[1:]
             elif fileStarted:
                 split_line = line.split(",")
                 id = split_line[0].rstrip()
@@ -85,37 +109,54 @@ def readFile(file, targets):
                 elif not id+file in targets:
                     targets[id+file] = parseLine(line, refLat, refLong, file)
             elif "ReferenceLongitude" in line:
-                refLong = Decimal(line.split("=")[1])
+                refLong = float(line.split("=")[1])
             elif "ReferenceLatitude" in line:
-                refLat = Decimal(line.split("=")[1])
-
+                refLat = float(line.split("=")[1])
 parser = argparse.ArgumentParser(description='Get targets')
 parser.add_argument('--tacviewFile', metavar='tacviewFile', nargs='*', default=['H:/Tacview.txt.acmi',
-                                                                                'H:/1.acmi',
-                                                                                'H:/2.acmi',
-                                                                                'H:/3.acmi',
-                                                                                'H:/4.acmi',
-                                                                                'H:/5.acmi'],
+                                                                                #'H:/1.acmi',
+                                                                                #'H:/2.acmi',
+                                                                                #'H:/3.acmi',
+                                                                                #'H:/4.acmi',
+                                                                                #'H:/5.acmi'
+                                                                                ],
                     help='unzipped tacview file')
-parser.add_argument('--json', metavar='json', nargs='?',
-                    help='json')
+parser.add_argument('--json', metavar='json', nargs='?', help='json')
+parser.add_argument('-o', metavar='longitude', type=float, nargs='?', help='an integer for the longitude', default='56.223681')
+parser.add_argument('-a', metavar='latitude', type=float, nargs='?', help='an integer for the latitude', default='27.131101')
+parser.add_argument('-p', help='only arguments', action='store_true')
+parser.add_argument('--target', default='a', const='a', nargs='?', choices=["a", "aa", "b"], help='list antiair, all, or building (default: %(default)s)')
+parser.add_argument('--dead', help='print dead units')
+parser.add_argument('--loop', dest='loop', action='store_true')
+parser.add_argument('--no-loop', dest='loop', action='store_false')
+parser.set_defaults(loop=False)
 args = parser.parse_args()
 targets = {}
 print(args.json)
-if args.json == "": 
+if args.json == None: 
     for file in args.tacviewFile:
         readFile(file, targets)
+        with open('data.json', 'w') as f:  # writing JSON object
+            json.dump(targets, f)
+            original_data = open('data.json', 'rb').read()  
+            compressed_data = zlib.compress(original_data, zlib.Z_BEST_COMPRESSION)
+            f = open('data.json.compressed', 'wb')  
+            f.write(compressed_data)  
+            f.close()
 else:
-    with open(args.json) as json_file:  
-        targets = json.load(json_file, cls=DecimalEncoder)
+    try:
+        json_file=open(args.json, "r")
+        targets = json.load(json_file)
+    except:
+        compressed_file=open(args.json+".compressed", "rb")
+        decompressed_data = zlib.decompress(compressed_file.read())
+        targets = json.load(decompressed_data)#TODO something is broken
 
-with open('data.json', 'w') as f:  # writing JSON object
-    json.dump(targets, f, cls=DecimalEncoder)
-    
-longmax = Decimal(0.0)
-longmin = Decimal(90.0)
-latmax = Decimal(0.0)
-latmin = Decimal(90.0)
+
+longmax = 0.0
+longmin = 90.0
+latmax = 0.0
+latmin = 90.0
 for target in targets.values():
     if longmax < target["long"]:
         longmax = target["long"]
@@ -126,39 +167,23 @@ for target in targets.values():
     if latmin > target["lat"]:
         latmin = target["lat"]
         #print(target)
-newTarget = True
-while newTarget :
-    lat = Decimal(input("Enter latitude for target in format " + str(latmin) + "-" + str(latmax) + ": ").replace(",","."))
-    #lat = Decimal(27.1024)
-    long = Decimal(input("Enter longitude for target in format " + str(longmin) + "-" + str(longmax) + ": ").replace(",","."))
-    #long = Decimal(56.1035)
-    targettype = input("Enter type a=all, aa=AntiAircraft, b=Building [aa]: ")
-    #targettype = "a"
-    if targettype == "":
-        targettype = "aa"
-    distanses = []
-    for target in targets.values():
-        if "Ground" in target["type"] or "Sea" in target["type"] :
-            found = False
-            
-            if targettype == "a" :
-                found = True
-            elif "AntiAircraft" in target["type"] and targettype == "aa" :
-                found = True
-            elif "Building" in target["type"] and targettype == "b" :
-                found = True
-            if found :
-                distance = ((target["lat"]-lat)**2+(target["long"]-long)**2).sqrt()
-                target["distance"]=distance
-                distanses.append(target)
-
-
-    distanses.sort(key = sortDist)
-
-    for distance in distanses:
-        if distance["distance"] < Decimal(0.1):
-        	
-            print(distance["status"]+", "+distance["type"]+", "+distance["name"]+", "+str(distance["lat"])+", "+str(distance["long"])+", "+str(distance["alt"])+" m, " + distance["group"]+", "+str(distance["distance"]))
-    
-    if input("New Target y/n: ") == "n":
-        newTarget = False
+if not args.p:
+    newTarget = True
+    while newTarget :
+        lat = float(input("Enter latitude for target in format " + str(latmin) + "-" + str(latmax) + ": ").replace(",","."))
+        #lat = Decimal(27.1024)
+        long = float(input("Enter longitude for target in format " + str(longmin) + "-" + str(longmax) + ": ").replace(",","."))
+        #long = Decimal(56.1035)
+        targettype = input("Enter type a=all, aa=AntiAircraft, b=Building [aa]: ")
+        #targettype = "a"
+        if targettype == "":
+            targettype = "aa"
+        printTarget(targets, long, lat, targettype, false)
+        
+        if input("New Target y/n: ") == "n":
+            newTarget = False
+else:
+    dead=True
+    if args.dead is None:
+        dead=False
+    printTarget(targets, args.o, args.a, args.target, dead)
